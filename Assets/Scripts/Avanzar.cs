e8f15c9a049e3c11ebae58ace4e074df01865c08
Fernando Paniagua <fernando.paniagua@gmail.com>
2020-11-17 09:55:41 +0100

    Postprocessing

---
+++ Assets/Scripts/Avanzar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avanzar : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.K)) speed = speed * -1;
+    }
+}
